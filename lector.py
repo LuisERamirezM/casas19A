f7211c5324a67b748f41940540cd54a158a49ad9
Luis Ramírez <luis.rmedina@alumnos.udg.mx>
2019-03-04 14:48:35 -0600

    Se creo el script para agregar todo el json a la base de datos

--- lector.py
+++ lector.py
@@ -2,45 +2,129 @@ import glob
 import json
 import mysql.connector
 
-conexion = mysql.connector.connect(user = 'michel', password = '12345', database = 'casas')
+conexion = mysql.connector.connect(user ='michel', password='12345', database='casas')
+
 cursor = conexion.cursor()
 
 files = glob.glob('*.json')
 
-tipo = {'venta':1, 'renta':2}
-origen = {'informador':1}
+tipo = {'venta': 1, 'renta': 2}
+origen = {'informador': 1}
+
+
+def existe_municipio(casa):
+    select = 'SELECT * FROM municipio  WHERE tipo = %s'
+    cursor.execute(select, (casa['ubicacion'],))
+
+    rows = cursor.fetchall()
+    if len(rows) > 0:
+        return True
+    else:
+        return False
+
+def insertar_municipio(casa):
+    insert = 'INSERT INTO municipio(tipo) VALUES(%s)'
+    cursor.execute(insert, (casa['ubicacion'],))
+    conexion.commit()
+    id_muni = cursor.lastrowid
+
+    return id_muni
+
+def existe_colonia(casa):
+    select = 'SELECT * FROM colonia WHERE nombre = %s'
+    cursor.execute(select,(casa['colonia'],))
+
+    rows = cursor.fetchall()
+    if len(rows) > 0:
+        for colonia in rows:
+            if colonia[2] == get_id_municipio(casa['ubicacion']):
+                return True
+    else:
+        return False
+
+def insertar_colonia(casa, id_muni):
+    insert = 'INSERT INTO colonia(nombre, id_municipio) VALUES(%s, %s)'
+    cursor.execute(insert, (casa['colonia'], id_muni))
+    conexion.commit()
+    return cursor.lastrowid
+
+def existe_fecha(file):
+    select = 'SELECT * FROM fecha WHERE fecha = %s'
+    cursor.execute(select, (file[:-5],))
+    rows = cursor.fetchall()
+    if len(rows) > 0:
+        return True
+    else:
+        return False
+
+def insertar_fecha(file):
+    insert = 'INSERT INTO fecha(fecha) VALUES(%s)'
+    cursor.execute(insert, (file[:-5],))
+    conexion.commit()
+    return cursor.lastrowid
+
+def insertar_bienraiz(casa,id_colonia,id_fecha):
+    insert = 'INSERT INTO bienraiz(titulo, precio, m2, rooms, baths, cars, descripcion, id_tipo, id_origen, id_colonia, id_fecha)\
+                     VALUES(%s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s)'
+    cursor.execute(insert, (casa['titulo'],
+                            casa['precio'],
+                            casa['m2'],
+                            casa['recamaras'],
+                            casa['wc'],
+                            casa['cars'],
+                            casa['descripcion'],
+                            1,
+                            1,
+                            id_colonia,
+                            id_fecha));
+    conexion.commit()
+
+
+def get_id_colonia(colonia, municipio):
+    select = 'SELECT * from colonia WHERE nombre = %s'
+    cursor.execute(select, (colonia,))
+
+    rows = cursor.fetchall()
+    for colonia in rows:
+        if colonia[2] == get_id_municipio(municipio):
+            return colonia[0]
+
+
+def get_id_fecha(fecha):
+    select = 'SELECT  id FROM fecha WHERE fecha = %s'
+    cursor.execute(select, (fecha,))
+
+    rows = cursor.fetchall()
+    return rows[0][0]
+
+def get_id_municipio(municipio):
+    select = 'SELECT id FROM municipio WHERE tipo = %s'
+    cursor.execute(select, (municipio,))
+    rows = cursor.fetchall()
+    return rows[0][0]
+
+
 
 for file in files:
     with open(file, encoding = 'utf-8') as f:
         casas = json.load(f)
         for casa in casas:
-            # print(casa['ubicacion'])
-            select = 'SELECT * FROM municipio WHERE tipo = %s'
-            cursor.execute(select, (casa['ubicacion'], ))
-            if len(cursor.fetchall()) == 0:
-                insert = 'INSERT INTO municipio(tipo) VALUES(%s)'
-                cursor.execute(insert, (casa[ 'ubicacion'], ))
-
-                id_muni = cursor.lastrowid
-                insert = 'INSERT INTO colonia(nombre, id_municipio) VALUES(%s, %s)'
-                cursor.execute(insert, (casa['colonia'],id_muni))
-                # print(cursor.lastrowid)
-                id_colonia = cursor.lastrowid
-                    # conexion.commit()
-
-                select = 'SELECT * FROM fecha WHERE fecha = %s'
-                cursor.execute(select, (file[:-5], ))
+                id_muni = 0
+                id_col = 0
                 id_fecha = 0
-                rows = cursor.fetchall()
-                if len(cursor.fetchall()) == 0:
-                     insert = 'INSERT INTO fecha(fecha) VALUES(%s)'
-                    cursor.execute(insert, (file[:-5], ))
-                    cursor.execute(insert, (file[:-5], ))
-                    conexion.commit()
-                    id_fecha = cursor.lastrowid
-                 else:
-                   id_fecha = rows[0][0]
 
-                 insert = 'INSERT INTO bienraiz(titulo, precio, m2, rooms, baths, cars, descripcion, id_tipo, id_origen, id_colonia, id_fecha) VALUES(%s, %s, %s, %s, %s, %s, %s,%s,%s, %s, %s)'
-                 cursor.execute(insert, (casa['titulo'],casa['precio'], casa['m2'], casa['recamaras'],casa['wc'], casa['cars'], casa['descripcion'], tipo[casa['tipo']], 1, id_colonia, id_fecha))
-                 conexion.commit()
+                if not existe_municipio(casa):
+                    id_muni = insertar_municipio(casa)
+                else:
+                    id_muni = get_id_municipio(casa['ubicacion'])
+                if not existe_colonia(casa):
+                    id_col = insertar_colonia(casa, id_muni)
+                else:
+                    id_col = get_id_colonia(casa['colonia'], casa['ubicacion'])
+
+                if not existe_fecha(file):
+                    id_fecha = insertar_fecha(file)
+                else:
+                    id_fecha = get_id_fecha(file[:-5])
+
+                insertar_bienraiz(casa, id_col,id_fecha)
